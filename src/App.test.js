06c3b012d5e71a2643cf99744f49e212d355248f
Yakusa26 <bertinim26@gmail.com>
2021-04-06 16:50:51 +0200

    Commit 06/04/2021 16h51

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Filter function", () => {
+  
+  test("Test du rendu de l'application", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/GAME OF LIFE - BERTINI & JAYRO/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
 });
+
